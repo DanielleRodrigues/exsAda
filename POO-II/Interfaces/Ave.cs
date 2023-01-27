2ae8d2fe4ce6e22ad001ee4484fbc9d99c652101
Danielle Rodrigues <daniellerodrigues-14@live.com>
2023-01-27 02:32:06 -0300

    some fixes

---
+++ POO-II/Interfaces/Ave.cs
@@ -0,0 +1,6 @@
+abstract class Ave
+{
+    public bool Rapina { get; set; }
+    public bool CorPena { get; set; }
+    public void Ciscar() { /* Implementation */ }
+}
---
+++ POO-II/Interfaces/Reptil.cs
@@ -0,0 +1,5 @@
+abstract class Reptil
+{
+    public bool TemEscamas { get; set; }
+    public bool TemCasco { get; set; }
+}
